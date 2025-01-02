24a7a574860b6e3381471a097477acafd2ed1b15
Ali Ahmad Farooq <aliahmad747426@gmail.com>
2025-01-02 10:20:23 +0500

    Add files via upload

--- config.js
+++ config.js
@@ -4,20 +4,31 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env'
 function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
+
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "mmYX0J5J#V49IPSNoEUMgBwDlx56Bfb3rbsW4TfzKgKc0R2dcDKI",
+SESSION_ID: process.env.SESSION_ID || "enter your session",
+CUSTOM_REACT: process.env.CUSTOM_REACT || "true",
+CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️‍🔥,❤️‍🩹,❤️,🩷,🧡,💛,💚,💙,🩵,💜,🤎,🖤,🩶,🤍",
+CAPTION: process.env.CAPTION || "*ᴘᴏᴡᴇʀᴇᴅ ʙʏ 𓄂𝕚𝕥𝕩.𝑺𝑯𝑬𝑰𝑲𝑯 𝑨𝑳𝑰 🔥༽༼*",
+ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+READ_MESSAGE: process.env.READ_MESSAGE || "true", // Added auto-read configuration
+AUTO_TYPING: process.env.AUTO_TYPING || "true",
+CURRENT_STATUS: process.env.CURRENT_STATUS || "false",
 MODE: process.env.MODE || "public",
-AUTO_VOICE: process.env.AUTO_VOICE  === undefined ? 'false' : process.env.AUTO_VOICE, 
+AUTO_VOICE: process.env.AUTO_VOICE || "false",
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 AUTO_REPLY: process.env.AUTO_REPLY || "false",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/KFYzSLs/Picsart-24-11-23-16-00-15-845.jpg",
-ALIVE_MSG: process.env.ALIVE_MSG || "HI DEAR I'M ONLINE I'M 𓄂𝑺𝑯𝑬𝑰𝑲𝑯 𝑨𝑳𝑰 🔥༽༼ WHATSAPP BOT 😊♻️",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/hVndwHm/6995.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || "HI DEAR I'M ONLINE I'M 𓄂𝑺𝑯𝑬𝑰𝑲𝑯 𝑨𝑳𝑰 🔥༽༼ WHATSAPP BOT 😊",
 ANTI_LINK: process.env.ANTI_LINK || "true",
 ANTI_BAD: process.env.ANTI_BAD || "true",
-PREFIX: process.env.PREFIX || "$",
-FAKE_RECORDING: process.env.FAKE_RECORDING || "true",
+PREFIX: process.env.PREFIX || ".",
+FAKE_RECORDING: process.env.FAKE_RECORDING || "false",
 AUTO_REACT: process.env.AUTO_REACT || "false",
 OWNER_REACT: process.env.OWNER_REACT || "false",
-BOT_NAME: process.env.BOT_NAME || "✦ꜱʜᴇɪᴋʜ-ᴀʟɪ-ᴍᴅ✦",
+BOT_NAME: process.env.BOT_NAME || "❖ꜱʜᴇɪᴋʜ-ᴀʟɪ-ᴍᴅ❖",
+AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "true",
+AUTO_STATUS__MSG: process.env.AUTO_STATUS__MSG || "`ʏᴏᴜʀ sᴛᴀᴛᴜs sᴇᴇɴ ᴊᴜsᴛ ɴᴏᴡ ʙʏ ♥️🌸⃝𝐂𝐮𝐭𝐞 𝐬𝐡𝐞𝐢𝐤𝐡ᥫ᭡👑`",
+OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39", // omdbapi.com
 };
