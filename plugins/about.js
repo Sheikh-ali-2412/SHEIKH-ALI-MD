a96d46236ee5165e59c3a4c472bbd819d3089465
Ali Ahmad Farooq <aliahmad747426@gmail.com>
2025-01-05 01:03:50 +0500

    Add files via upload

--- plugins/about.js
+++ plugins/about.js
@@ -1,8 +1,36 @@
-/* 
+/*
+
+                                                                                                                                                                                             
+                                                                                                                                                                                             
+   SSSSSSSSSSSSSSS HHHHHHHHH     HHHHHHHHHEEEEEEEEEEEEEEEEEEEEEEIIIIIIIIIIKKKKKKKKK    KKKKKKKHHHHHHHHH     HHHHHHHHH                    AAA               LLLLLLLLLLL             IIIIIIIIII
+ SS:::::::::::::::SH:::::::H     H:::::::HE::::::::::::::::::::EI::::::::IK:::::::K    K:::::KH:::::::H     H:::::::H                   A:::A              L:::::::::L             I::::::::I
+S:::::SSSSSS::::::SH:::::::H     H:::::::HE::::::::::::::::::::EI::::::::IK:::::::K    K:::::KH:::::::H     H:::::::H                  A:::::A             L:::::::::L             I::::::::I
+S:::::S     SSSSSSSHH::::::H     H::::::HHEE::::::EEEEEEEEE::::EII::::::IIK:::::::K   K::::::KHH::::::H     H::::::HH                 A:::::::A            LL:::::::LL             II::::::II
+S:::::S              H:::::H     H:::::H    E:::::E       EEEEEE  I::::I  KK::::::K  K:::::KKK  H:::::H     H:::::H                  A:::::::::A             L:::::L                 I::::I  
+S:::::S              H:::::H     H:::::H    E:::::E               I::::I    K:::::K K:::::K     H:::::H     H:::::H                 A:::::A:::::A            L:::::L                 I::::I  
+ S::::SSSS           H::::::HHHHH::::::H    E::::::EEEEEEEEEE     I::::I    K::::::K:::::K      H::::::HHHHH::::::H                A:::::A A:::::A           L:::::L                 I::::I  
+  SS::::::SSSSS      H:::::::::::::::::H    E:::::::::::::::E     I::::I    K:::::::::::K       H:::::::::::::::::H               A:::::A   A:::::A          L:::::L                 I::::I  
+    SSS::::::::SS    H:::::::::::::::::H    E:::::::::::::::E     I::::I    K:::::::::::K       H:::::::::::::::::H              A:::::A     A:::::A         L:::::L                 I::::I  
+       SSSSSS::::S   H::::::HHHHH::::::H    E::::::EEEEEEEEEE     I::::I    K::::::K:::::K      H::::::HHHHH::::::H             A:::::AAAAAAAAA:::::A        L:::::L                 I::::I  
+            S:::::S  H:::::H     H:::::H    E:::::E               I::::I    K:::::K K:::::K     H:::::H     H:::::H            A:::::::::::::::::::::A       L:::::L                 I::::I  
+            S:::::S  H:::::H     H:::::H    E:::::E       EEEEEE  I::::I  KK::::::K  K:::::KKK  H:::::H     H:::::H           A:::::AAAAAAAAAAAAA:::::A      L:::::L         LLLLLL  I::::I  
+SSSSSSS     S:::::SHH::::::H     H::::::HHEE::::::EEEEEEEE:::::EII::::::IIK:::::::K   K::::::KHH::::::H     H::::::HH        A:::::A             A:::::A   LL:::::::LLLLLLLLL:::::LII::::::II
+S::::::SSSSSS:::::SH:::::::H     H:::::::HE::::::::::::::::::::EI::::::::IK:::::::K    K:::::KH:::::::H     H:::::::H       A:::::A               A:::::A  L::::::::::::::::::::::LI::::::::I
+S:::::::::::::::SS H:::::::H     H:::::::HE::::::::::::::::::::EI::::::::IK:::::::K    K:::::KH:::::::H     H:::::::H      A:::::A                 A:::::A L::::::::::::::::::::::LI::::::::I
+ SSSSSSSSSSSSSSS   HHHHHHHHH     HHHHHHHHHEEEEEEEEEEEEEEEEEEEEEEIIIIIIIIIIKKKKKKKKK    KKKKKKKHHHHHHHHH     HHHHHHHHH     AAAAAAA                   AAAAAAALLLLLLLLLLLLLLLLLLLLLLLLIIIIIIIIII
+                                                                                                                                                                                             
+                                                                                                                                                                                             
+                                                                                                                                                                                             
+                                                                                                                                                                                             
+                                                                                                                                                                                             
+                                                                                                                                                                                             
+                                                                                                                                                                                             
+                                                                                                                                                                                               
 created by 𓄂𝕚𝕥𝕩.𝑺𝑯𝑬𝑰𝑲𝑯 𝑨𝑳𝑰 🔥༽༼ 🕵
 contact me 923143702270 ♻️
 © Copy coder alert ⚠
 */
+
 
 const config = require('../config')
 const {cmd , commands} = require('../command')
@@ -20,6 +48,9 @@ let about = `*╭┈───────────────•*
 *👋 HELLO ${pushname}*
 
 *╰┈───────────────•*
+
+*[ • 𝐒𝐇𝐄𝐈𝐊𝐇-𝐀𝐋𝐈-𝐌𝐃 - 𝐎𝐖𝐍𝐄𝐑 • ]*
+
 *╭┈───────────────•*
 *│  ◦* *ᴡᴇʟᴄᴏᴍᴇ ɪᴛs sʜᴇɪᴋʜ-ᴀʟɪ-ᴍᴅ-ʙᴏᴛ*
 *│  ◦* *ᴄʀᴇᴀᴛᴇʀ ᴀʟɪ ᴀʜᴍᴀᴅ...²⁴⁰⁶*
@@ -27,9 +58,7 @@ let about = `*╭┈───────────────•*
 *│  ◦* *ᴘᴜʙʟɪᴄ ɴᴀᴍᴇ➠ sʜᴇɪᴋʜ sᴀʙ*
 *│  ◦* *ᴀɢᴇ➠ ɴɪɴᴇᴛᴇᴇɴ ʏᴇᴀʀ*
 *│  ◦* *ᴄɪᴛʏ➠ ᴘᴇʀsɴᴏʟ ʜᴀɪ*
-*│  ◦* *ᴀ sɪᴍᴘʟᴇ ᴡʜᴀᴛsᴀᴘᴘ ʙᴏᴛ ᴅᴇᴠᴇʟᴘᴏʀ*
 *╰┈───────────────•*
-> *◆◆◆◆◆◆◆◆◆◆◆◆*
 *•────────────•⟢*
 > © ᴘᴏᴡᴇʀᴇᴅ ʙʏ 𓄂𝕚𝕥𝕩.𝑺𝑯𝑬𝑰𝑲𝑯 𝑨𝑳𝑰 🔥༽༼
 *•────────────•⟢*
